b19e661a5df32b17042cdfd067451ed82788ad95
Ourai Lin <ourairyu@gmail.com>
2024-04-04 09:54:42 +0800

    build: generate and deploy via KnoSys on Thu Apr 04 2024 09:54:42 GMT+0800 (China Standard Time)

--- bin/helper.js
+++
@@ -1,6 +0,0 @@
-const { resolve: resolvePath } = require('path');
-const { readFileSync } = require('fs');
-
-const rootPath = resolvePath(__dirname, '..');
-
-module.exports = require(resolvePath(rootPath, JSON.parse(readFileSync(resolvePath(rootPath, '.knosys/config.json'), 'utf8').toString().trim()).$path));
